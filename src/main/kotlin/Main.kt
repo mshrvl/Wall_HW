d23fb5338abf2199db09abfc795efe54cb442e59
Mishurin V <mshrvl17@gmail.com>
2023-12-20 19:21:23 +0300

    HW task 1 and 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,46 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+data class Post(
+    val reposts: Int,
+    val owner_id: Int,
+    val date: Int,
+    val text: String,
+    val friend_only: Boolean,
+    val signer_id: Int,
+    val can_pin: Boolean,
+    val can_delete: Boolean,
+    val can_edit: Boolean,
+    val is_favourite: Boolean
+)
+object Likes {
+    data class LikesInfo(
+        val count: Int,
+        val userLikes: Boolean,
+        val canLike: Boolean,
+        val canPublish: Boolean,
+    )
+}
+object WallService {
+    private var posts = emptyArray<Post>()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    fun add(post: Post): Post {
+        posts += post
+        return post
+    }
+
+    private var id = 0
+
+    fun generateId(): Int {
+        id++
+        return id
+    }
+}
+
+}
+fun main() {}
+
+fun add(post: Post) {
+    val post = Post(0,2,11,"TEST",true, 4,true,true,true,false)
+
+}
+
+
 }
